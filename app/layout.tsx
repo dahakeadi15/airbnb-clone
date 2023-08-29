05388bf97ad4c03199dbc45d4e614191e7d10ba6
Aditya Dahake <dahakeadi15@gmail.com>
2023-08-29 16:18:53 +0530

    create navbar and add logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Nunito } from 'next/font/google';
 import type { Metadata } from 'next';
 
 import './globals.css';
+import Navbar from './components/navbar/Navbar';
 
 export const metadata: Metadata = {
   title: 'Airbnb',
@@ -20,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={nunito.className}>
+        <Navbar />
         {children}
       </body>
     </html>
