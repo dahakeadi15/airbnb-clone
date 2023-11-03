953eb85cc1b36fdcb50e8f895a21d3b92fd89d15
Aditya Dahake <dahakeadi15@gmail.com>
2023-11-03 21:08:40 +0530

    add upload images input to rent modal

---
+++ app/components/inputs/ImageUpload.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { CldUploadWidget } from "next-cloudinary";
+import Image from "next/image";
+import { useCallback } from "react";
+import { TbPhotoPlus } from "react-icons/tb";
+
+declare global {
+  var cloudinary: any;
+}
+
+interface ImageUploaProps {
+  onChange: (value: string) => void;
+  value: string;
+}
+
+const ImageUpload: React.FC<ImageUploaProps> = ({ onChange, value }) => {
+  const handleUpload = useCallback(
+    (result: any) => {
+      onChange(result.info.secure_url);
+    },
+    [onChange]
+  );
+
+  return (
+    <CldUploadWidget
+      onUpload={handleUpload}
+      uploadPreset="q7nggduh"
+      options={{
+        maxFiles: 1,
+      }}
+    >
+      {({ open }) => {
+        return (
+          <div
+            onClick={() => open?.()}
+            className="
+              relative
+              cursor-pointer
+              hover:opacity-70
+              transition
+              border-dashed
+              border-2
+              p-20
+              border-neutral-300
+              flex
+              flex-col
+              justify-center
+              items-center
+              gap-4
+              text-neutral-500
+            "
+          >
+            <TbPhotoPlus size={50} />
+            <div className="font-semibold text-lg">Click to upload</div>
+            {value && (
+              <div>
+                <Image
+                  alt="Upload"
+                  fill
+                  style={{ objectFit: "cover" }}
+                  src={value}
+                />
+              </div>
+            )}
+          </div>
+        );
+      }}
+    </CldUploadWidget>
+  );
+};
+
+export default ImageUpload;
