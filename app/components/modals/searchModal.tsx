ec7aeea5f9905849254b5d554f4f5ec070399e97
Aditya Dahake <dahakeadi15@gmail.com>
2023-11-07 08:46:16 +0530

    Update search modal to display search parameters

--- app/components/modals/searchModal.tsx
+++ app/components/modals/searchModal.tsx
@@ -66,7 +66,7 @@ const SearchModal = () => {
 
     const updatedQuery: any = {
       ...currentQuery,
-      location: location?.value,
+      locationValue: location?.value,
       guestCount,
       roomCount,
       bathroomCount,
